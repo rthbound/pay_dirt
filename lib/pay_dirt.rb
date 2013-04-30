b06cd46a7f97f659014e93046a12bb4f40549ecf
Tad Hosford <tad.hosford@gmail.com>
2013-04-30 15:05:37 -0500

    Adds require statements back to pay_dirt.rb

---
+++ lib/pay_dirt.rb
@@ -0,0 +1,3 @@
+require_relative "#{File.dirname(__FILE__)}/pay_dirt/use_case.rb"
+require_relative "#{File.dirname(__FILE__)}/pay_dirt/result.rb"
+require_relative "#{File.dirname(__FILE__)}/pay_dirt/base.rb"
