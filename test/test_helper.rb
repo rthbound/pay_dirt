4a81796a234ac2b468a99f53fc2d094e1d8a3a3c
Ryan T. Hosford <tad.hosford@gmail.com>
2013-10-30 12:50:03 -0500

    Make sure travis is testing our thor script

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,3 +7,15 @@ require "minitest/autorun"
 $: << File.dirname(__FILE__) + "/../lib"
 $: << File.dirname(__FILE__)
 
+require_relative "../lib/quick/digit_check" if File.file?("lib/quick/digit_check.rb")
+
+MiniTest::Unit.after_tests do
+  # Refresh generated code for next run
+  `rm -rf lib/quick`       if File.file? "lib/quick/digit_check.rb"
+  `rm -rf test/unit/quick` if File.file? "test/unit/quick/digit_check_test.rb"
+  `thor pay_dirt:service_object:new quick/digit_check -d fingers toes nose -D fingers:10 toes:10`
+
+  # Break build if test files are not generated
+  (File.file?("lib/quick/digit_check.rb") && File.file?("test/unit/quick/digit_check_test.rb")) or raise
+end
+require "quick/digit_check"
