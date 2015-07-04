81c6f5e250dc6effa0e56c5f06ba087f88edb64b
Ryan Hosford <tad.hosford@gmail.com>
2015-07-04 02:02:35 -0500

    Documenting PayDirt::Base

--- lib/pay_dirt/base.rb
+++ lib/pay_dirt/base.rb
@@ -1,7 +1,10 @@
 require_relative 'use_case'
 
-# Here's something to inherit from
 module PayDirt
+  # A developer can optionally inherit from this base class, rather than include the PayDirt::UseCase module --
+  # this is obviously not possible if your service object's class is already inheriting from another class.
+  #
+  # @since 0.0.0
   class Base
     include PayDirt::UseCase
   end
