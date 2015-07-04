60a7e906b81a116c42353eff292346bb18b45004
Ryan Hosford <tad.hosford@gmail.com>
2015-07-04 02:06:20 -0500

    Gets rid of unnecessary Module#included usage
      - Also, documentation work

--- lib/pay_dirt/use_case.rb
+++ lib/pay_dirt/use_case.rb
@@ -1,51 +1,52 @@
 module PayDirt
+  # Provides the basic functionality every PayDirt service object should have.
+  # @since 0.0.2
   module UseCase
-    def self.included(base)
-      # Load instance variables from the provided hash of dependencies.
-      #
-      # Raises if any required dependencies are missing from +options+ hash.
-      #
-      # @param [List<String,Symbol>]
-      #   option_names list of keys representing required dependencies
-      #
-      # @param [Hash]
-      #   options A hash of dependencies
-      #
-      # @public
-      def load_options(*option_names, options)
-        # Load required options
-        option_names.each { |o| options = load_option(o, options) }
+    # Load instance variables from the provided hash of dependencies.
+    #
+    # Raises if any required dependencies (+required_options+) are missing from +options+ hash.
+    #
+    # @param [List<String,Symbol>]
+    #   option_names list of keys representing required dependencies
+    #
+    # @param [Hash]
+    #   options A hash of dependencies
+    #
+    # @public
+    def load_options(*required_options, options)
+      # Load required options
+      required_options.each { |o| options = load_option(o, options) }
 
-        # Load remaining options
-        options.each_key  { |o| options = load_option(o, options) }
-      end
+      # Load remaining options
+      options.each_key  { |k| options = load_option(k, options) }
+    end
 
-      # Returns a result object representing success.
-      #
-      # @param [List<String,Symbol>]
-      #   success should the result be +#successful?+
-      #
-      # @param [Object]
-      #   data (nil) optional, an object containing information
-      #
-      # @public
-      def result(success, data = nil)
-        PayDirt::Result.new(success: success, data: data)
-      end
+    # Returns a result object conveying success or failure (+success+)
+    # and any +data+. See PayDirt::Result.
+    #
+    # @param [Boolean]
+    #   success should the result be +#successful?+?
+    #
+    # @param [Object]
+    #   data (nil) optional, an object containing information
+    #
+    # @public
+    def result(success, data = nil)
+      PayDirt::Result.new(success: success, data: data)
+    end
 
-      private
-      # @private
-      def load_option(option, options)
-        instance_variable_set("@#{option}", fetch(option, options))
-        options.delete(option)
+    private
+    # @private
+    def load_option(option, options)
+      instance_variable_set("@#{option}", fetch(option, options))
+      options.delete(option)
 
-        return options
-      end
+      return options
+    end
 
-      # @private
-      def fetch(opt, opts)
-        opts.fetch(opt.to_sym) { raise "Missing required option: #{opt}" }
-      end
+    # @private
+    def fetch(opt, opts)
+      opts.fetch(opt.to_sym) { raise "Missing required option: #{opt}" }
     end
   end
 end
